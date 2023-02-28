50f66e8329e030d0b8ddb0cf738f24fdd34d955e
hgrafa <hugorafell@gmail.com>
2023-02-27 21:35:09 -0300

    adding simulate annealing implementation

--- heuristica/heuristica/main.py
+++ heuristica/heuristica/main.py
@@ -1,76 +1,138 @@
 import model.Equipe as Equipe
 
 from util.Processador import Processador
+from util.Trocador import Trocador
 from util.FileManipulator import FileManipulator
 from cmath import exp as complex_exp
 from random import uniform as uniform_random
 
-def exp(number):
-  return complex_exp(number).real
+# ------------------------------------
+# UTILITÁRIO
 
-dados = FileManipulator('instancia3.txt')
 
-processador = Processador(dados)
-processador.inicializar_equipes()
+def exp(number): return complex_exp(number).real
 
-while processador.processar_proximo() :
-  pass
+# ------------------------------------
 
-print(processador)
-# maiores -> menores -> realoca
-# critério (energia/função objetivo) -> equipe.tempo_de_processamento
-# 
 
-def initial_solution() :
-  dados = FileManipulator('instancia3.txt')
-  processador = Processador(dados)
-  processador.inicializar_equipes()
-  while processador.processar_proximo() :
-    continue
-  
-  return processador.equipes
+def initial_solution():
+    dados = FileManipulator('instancia11.txt')
+    processador = Processador(dados)
+    processador.inicializar_equipes()
+    while processador.processar_proximo():
+        continue
 
-def generate_neighbor(equipes):
-  alternative_solution = equipes
+    print(processador)
+    return processador.equipes, dados.configuracoes
 
-def energy(equipe):
-  if not isinstance(equipe, Equipe):
-    pass
+# ------------------------------------
 
-  return equipe.tempo_de_processamento
 
-def simulated_annealing(temperature, cooling_rate, max_iterations):
+def generate_neighbor(equipes, configuracoes):
+    copia_equipes = equipes.copy()
+    neighbor_generator = Trocador(copia_equipes, configuracoes)
+    neighbor_generator.realiza_troca()
+    return copia_equipes
+
+# ------------------------------------
+
+
+def energy(equipes):
+    maior_tempo = -1
+
+    for equipe in equipes:
+        if equipe.tempo_de_processamento > maior_tempo:
+            maior_tempo = equipe.tempo_de_processamento
+
+    return maior_tempo
+
+# ------------------------------------
+
+
+def simulated_annealing(temperature, cooling_rate, freeze_temperature, max_iterations):
     # machines: lista de máquinas
-    # es
-    # temperature: temperatura iniciateams: lista de equipl
-    # cooling_rate: taxa de resfriamento
+    # temperature: temperatura inicial
+    # teams: lista de equipe
+    # cooling_rate: taxa de resfriamento (0, 1) - decimal value
     # max_iterations: número máximo de iterações
 
     # Inicializa a solução atual com uma distribuição aleatória de máquinas em equipes
-    current_solution = initial_solution()
+    base_solution, teams_configuration = initial_solution()
+    current_solution = base_solution
     best_solution = current_solution
 
     # Loop principal
-    for i in range(max_iterations):
-        # Gera uma nova solução a partir da solução atual
-        new_solution = generate_neighbor(current_solution)
+    while temperature > freeze_temperature:
+        for _ in range(max_iterations):
+            # Gera uma nova solução a partir da solução atual
+            new_solution = generate_neighbor(
+                current_solution, teams_configuration)
 
-        # Calcula a diferença de energia entre a nova solução e a solução atual
-        energy_delta = energy(new_solution) - energy(current_solution)
+            # Calcula a diferença de energia entre a nova solução e a solução atual
+            energy_delta = energy(new_solution) - energy(current_solution)
 
-        # Se a nova solução é melhor, aceita-a como a solução atual
-        if energy_delta < 0:
-            current_solution = new_solution
-            # Atualiza a melhor solução se a nova solução for ainda melhor
-            if energy(new_solution) < energy(best_solution):
-                best_solution = new_solution
-        else:
-            # Senão, aceita a nova solução com uma probabilidade determinada pela temperatura
-            p = uniform_random(0, 1)
-            if p < exp(-energy_delta / temperature):
+            # Se a nova solução é melhor, aceita-a como a solução atual
+            if energy_delta < 0:
                 current_solution = new_solution
+                # Atualiza a melhor solução se a nova solução for ainda melhor
+                if energy(new_solution) < energy(best_solution):
+                    best_solution = new_solution
+            else:
+                # Senão, aceita a nova solução com uma probabilidade determinada pela temperatura
+                p = uniform_random(0, 1)
+                if p < exp(-energy_delta / temperature):
+                    current_solution = new_solution
 
         # Reduz a temperatura
         temperature = temperature * cooling_rate
 
-    return best_solution
+    return base_solution, best_solution
+
+# ------------------------------------
+
+
+inicial, final = simulated_annealing(100, 0.6, 20, 10)
+
+print("--------------------------------------------")
+print("--------------------------------------------")
+print("--------------------------------------------")
+print("--------------------------------------------")
+print("--------------------------------------------")
+print("--------------------------------------------")
+print("--------------------------------------------")
+print("--------------------------------------------")
+print("--------------------------------------------")
+print("--------------------------------------------")
+print("--------------------------------------------")
+print("--------------------------------------------")
+
+for equipe in final:
+    print(equipe)
+    print("-----------------")
+
+print("--------------------------------------------")
+print("--------------------------------------------")
+print("--------------------------------------------")
+print("--------------------------------------------")
+print("--------------------------------------------")
+print("--------------------------------------------")
+print("--------------------------------------------")
+print("--------------------------------------------")
+print("--------------------------------------------")
+print("--------------------------------------------")
+print("--------------------------------------------")
+print("--------------------------------------------")
+
+tempo_max_inicial = -1
+tempo_max_final = -1
+
+for equipe in inicial:
+    if equipe.tempo_de_processamento > tempo_max_inicial:
+        tempo_max_inicial = equipe.tempo_de_processamento
+
+for equipe in final:
+    if equipe.tempo_de_processamento > tempo_max_final:
+        tempo_max_final = equipe.tempo_de_processamento
+
+print(f"TEMPO MAX INICIAL: {tempo_max_inicial}")
+print(f"TEMPO MAX FINAL: {tempo_max_final}")
--- heuristica/heuristica/util/Trocador.py
+++ heuristica/heuristica/util/Trocador.py
@@ -1,95 +1,98 @@
-
-import util.Filtro as Filtro
-import model.Maquina as Maquina
+from model.Maquina import Maquina
 from random import randint as posicao_aleatoria
+
 
 class Trocador:
 
-  def __init__(self, equipes, configuracoes):
-    self.configuracoes = configuracoes
-    self.equipes = equipes
-    self.equipes_ordenadas = sorted(equipes, key=lambda x: x.disponibilidade)
+    def __init__(self, equipes, configuracoes):
+        self.configuracoes = configuracoes
+        self.equipes = equipes
+        self.equipes_ordenadas = sorted(
+            equipes, key=lambda x: x.disponibilidade)
 
-  # [ 12, 14, 15, 20 ]
-  #    ^  ^      
-  # ORDENAÇÃO: n*log(n)
-  # COMPLEXIDADE: (n)(n-1)(n*log(n))/2
-  # IDENTIFICAR: condição de parada
-  # PROBLEMA: loop de trocas
-  # Ordena e captura disponiveis por disponibilidade e configuração
+    # [ 12, 14, 15, 20 ]
+    #    ^  ^
+    # ORDENAÇÃO: n*log(n)
+    # COMPLEXIDADE: (n)(n-1)(n*log(n))/2
+    # IDENTIFICAR: condição de parada
+    # PROBLEMA: loop de trocas
+    # Ordena e captura disponiveis por disponibilidade e configuração
 
-  # maquina -> origem == ind. ativ -> config
+    # maquina -> origem == ind. ativ -> config
 
-  # ------------------------------------------
+    # ------------------------------------------
 
-  def realiza_troca(self):
-    self.ordena_equipes_por_disponibilidade()
-    equipe_origem = self.equipes[0]
-    maquina = self.pior_maquina_da_equipe(equipe_origem)
-    disponiveis = self.equipes_disponiveis(maquina)
+    def realiza_troca(self):
+        self.ordena_equipes_por_disponibilidade()
+        equipe_origem = self.equipes[0]
+        maquina = self.pior_maquina_da_equipe(equipe_origem)
+        disponiveis = self.equipes_disponiveis(maquina)
 
-    if len(disponiveis) == 0:
-      return
-      
-    pos_equipe_destino = posicao_aleatoria(0, len(disponiveis) - 1)
-    equipe_destino = disponiveis[pos_equipe_destino]
+        if len(disponiveis) == 0:
+            return
 
-    equipe_origem.maquinas.remove(maquina)
-    equipe_destino.maquinas.append(maquina)
+        pos_equipe_destino = posicao_aleatoria(0, len(disponiveis) - 1)
+        equipe_destino = disponiveis[pos_equipe_destino]
 
-  # ------------------------------------------
+        equipe_origem.maquinas.remove(maquina)
+        equipe_destino.maquinas.append(maquina)
 
-  def equipes_disponiveis(self, maquina):
+    # ------------------------------------------
 
-    disponiveis_por_disponibilidade = self.captura_equipes_com_disponibilidade(maquina)
-    disponiveis_por_configuracao = self.captura_equipes_por_configuracao(maquina)
+    def ordena_equipes_por_disponibilidade(self):
+        def criterio_por_disponibilidade(equipe): return equipe.disponibilidade
+        # * o sorted ordena de modo crescente por disponibilidade
+        # ! TODO checar se a melhor ordem realmente é a reversed
+        return sorted(self.equipes, key=criterio_por_disponibilidade, reverse=True)
 
-    comum = lambda element: element in disponiveis_por_disponibilidade
-    interseccao = list(filter(comum, disponiveis_por_configuracao))
+    # ------------------------------------------
 
-    return interseccao
+    def equipes_disponiveis(self, maquina):
 
-  # ------------------------------------------
+        disponiveis_por_disponibilidade = self.captura_equipes_com_disponibilidade(
+            maquina)
+        disponiveis_por_configuracao = self.captura_equipes_por_configuracao(
+            maquina)
 
-  def captura_equipes_com_disponibilidade(self, maquina):
-    disponiveis = []
-    configuracao = self.configuracoes[maquina.atividade]
-      
-    for i in range(len(configuracao)):
-      valor = configuracao[i]
+        def comum(element): return element in disponiveis_por_disponibilidade
+        interseccao = list(filter(comum, disponiveis_por_configuracao))
 
-      if valor == 1:
-        disponiveis.append(self.equipes[i])
+        return interseccao
 
-    return disponiveis
+    # ------------------------------------------
 
-  # ------------------------------------------
+    def captura_equipes_com_disponibilidade(self, maquina):
+        disponiveis = []
+        configuracao = self.configuracoes[maquina.atividade]
 
-  def captura_equipes_por_configuracao(self, maquina):
-    disponiveis = []
-      
-    for equipe in self.equipes():
-      if equipe.disponibilidade > maquina.tempo_processamento:
-        disponiveis.append(equipe)
+        for i in range(len(configuracao)):
+            valor = configuracao[i]
 
-    return disponiveis
+            if valor == 1:
+                disponiveis.append(self.equipes[i])
 
-  # ------------------------------------------
+        return disponiveis
 
-  def ordena_equipes_por_disponibilidade(self):
-    criterio_por_disponibilidade = lambda equipe: equipe.disponibilidade
-    # ! TODO checar se a melhor ordem realmente é a reversed
-    return sorted(self.equipes, criterio_por_disponibilidade, reverse = True)
+    # ------------------------------------------
 
-  # ------------------------------------------
+    def captura_equipes_por_configuracao(self, maquina):
+        disponiveis = []
 
-  def pior_maquina_da_equipe(self, equipe):
-    pior_maquina = equipe.maquinas[0]
+        for equipe in self.equipes:
+            if equipe.disponibilidade > maquina.tempo_processamento:
+                disponiveis.append(equipe)
 
-    for maquina in equipe.maquinas:
-      if maquina.tempo_de_processamento > pior_maquina.tempo_de_processamento:
-        pior_maquina = maquina
+        return disponiveis
 
-    return pior_maquina
+    # ------------------------------------------
 
-  # ------------------------------------------
+    def pior_maquina_da_equipe(self, equipe):
+        pior_maquina = equipe.maquinas[0]
+
+        for maquina in equipe.maquinas:
+            if maquina.tempo_processamento > pior_maquina.tempo_processamento:
+                pior_maquina = maquina
+
+        return pior_maquina
+
+    # ------------------------------------------
