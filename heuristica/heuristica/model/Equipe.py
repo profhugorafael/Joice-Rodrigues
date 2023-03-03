264000608eed1929ed0a9b3b6a38c0e0abd185af
hgrafa <hugorafell@gmail.com>
2023-03-03 07:43:37 -0300

    Atualizando models

--- heuristica/heuristica/model/Equipe.py
+++ heuristica/heuristica/model/Equipe.py
@@ -1,58 +1,78 @@
 from model.Maquina import Maquina
 
+
 def eh_maquina(elemento):
-  return True if isinstance(elemento, Maquina) else False
+    return True if isinstance(elemento, Maquina) else False
+
+
 class Equipe:
 
-  # construtor
-  def __init__(self, id,  disponibilidade, janela_final : list, janela_inicial : list ):
-    self.id = id
-    self.disponibilidade = disponibilidade
-    self.janela_final = janela_final.copy()
-    self.janela_inicial = janela_inicial.copy()
-    self.tempo_de_processamento = 0
-    self.maquinas = []
+    # construtor
+    def __init__(self, id,  disponibilidade, janela_final: list, janela_inicial: list):
+        self.id = id
+        self.disponibilidade = disponibilidade
+        self.janela_final = janela_final.copy()
+        self.janela_inicial = janela_inicial.copy()
+        # TODO ajustar tempo de processamento para um cálculo
+        self.tempo_de_processamento = 0
+        self.maquinas = []
 
-  def adiciona_maquina(self, nova_maquina):
-    if not eh_maquina(nova_maquina) : return
+    def adiciona_maquina(self, nova_maquina):
+        if not eh_maquina(nova_maquina):
+            return
 
-    self.maquinas.append(nova_maquina)
-    self.__ajusta_janela_inicial__(nova_maquina)
-    self.__ajusta_tempo_janela_final__(nova_maquina)
-    self.__ajusta_tempo_disponibilidade__(nova_maquina)
+        self.maquinas.append(nova_maquina)
+        self.__ajusta_janela_inicial__(nova_maquina)
+        self.__ajusta_tempo_janela_final__(nova_maquina)
+        self.__ajusta_tempo_disponibilidade__(nova_maquina)
 
-  # ------------------------------------------
+    # ------------------------------------------
 
-  def __ajusta_tempo_janela_final__(self, nova_maquina):
-    self.janela_final[nova_maquina.index] -= nova_maquina.tempo_processamento
-  
-  # ------------------------------------------
+    def __ajusta_tempo_janela_final__(self, nova_maquina):
+        self.janela_final[nova_maquina.index] -= nova_maquina.tempo_processamento
 
-  def __ajusta_tempo_disponibilidade__(self, nova_maquina):
-    self.disponibilidade -= nova_maquina.tempo_processamento
-  
-  # ------------------------------------------
+    # ------------------------------------------
 
-  def __ajusta_janela_inicial__(self, nova_maquina):
+    def __ajusta_tempo_disponibilidade__(self, nova_maquina):
+        self.disponibilidade -= nova_maquina.tempo_processamento
 
-    index_maquina = nova_maquina.index
-    janela = nova_maquina.tempo_processamento      
-    horario_autorizado = self.janela_inicial[index_maquina]
-    tempo_atual = self.tempo_de_processamento
+    # ------------------------------------------
 
-    # precisa esperar ?
-    tempo_ajustado = max(tempo_atual, horario_autorizado) + janela
-    self.tempo_de_processamento = tempo_ajustado
+    def __ajusta_janela_inicial__(self, nova_maquina):
 
-  # ------------------------------------------
-  
-  def __str__(self):
-    aux = f'EQUIPE # {self.id}\n'
-    aux += f'DISPONIBILIDADE: {self.disponibilidade}\n'
-    aux += f'TEMPO TOTAL {self.tempo_de_processamento}\n'
-    aux += f'MAQUINAS: \n'
+        index_maquina = nova_maquina.index
+        janela = nova_maquina.tempo_processamento
+        horario_autorizado = self.janela_inicial[index_maquina]
+        tempo_atual = self.tempo_de_processamento
 
-    for maquina in self.maquinas:
-      aux += '\t' + str(maquina) + '\n'
+        # precisa esperar ?
+        tempo_ajustado = max(tempo_atual, horario_autorizado) + janela
+        self.tempo_de_processamento = tempo_ajustado
 
-    return aux
+    # ------------------------------------------
+
+    def liberar_maquinas_para_troca(self):
+        for maquina in self.maquinas:
+            maquina.desmarcar()
+
+    # ------------------------------------------
+
+    def existe_equipe_valida(self):
+        for maquina in self.maquinas:
+            if not maquina.invalida:
+                return True
+
+        return False
+
+    # ------------------------------------------
+
+    def __str__(self):
+        aux = f'EQUIPE # {self.id}\n'
+        aux += f'DISPONIBILIDADE: {self.disponibilidade}\n'
+        aux += f'TEMPO TOTAL {self.tempo_de_processamento}\n'
+        aux += 'MAQUINAS: \n'
+
+        for maquina in self.maquinas:
+            aux += '\t' + str(maquina) + '\n'
+
+        return aux
--- heuristica/heuristica/model/Maquina.py
+++ heuristica/heuristica/model/Maquina.py
@@ -1,12 +1,25 @@
 class Maquina:
 
-  def __init__(self, tempo_processamento, indice_atividade, index_maquina):
-     self.tempo_processamento = tempo_processamento
-     self.atividade = indice_atividade
-     self.index = index_maquina
+    def __init__(self, tempo_processamento, indice_atividade, index_maquina):
+        self.tempo_processamento = tempo_processamento
+        self.atividade = indice_atividade
+        self.index = index_maquina
+        self.invalida = False
 
-  def description(self) :
-    print('my description')
+    def __str__(self):
+        return f'tempo de processamento {self.tempo_processamento} | indice atividade : {self.atividade} | maquina de origem : {self.index}'
 
-  def __str__(self):
-    return f'tempo de processamento {self.tempo_processamento} | indice atividade : {self.atividade} | maquina de origem : {self.index}'
+    def atribuir_equipe(self, equipe):
+        self.equipe = equipe
+        self.atribuida = True
+
+    def reatribuir_equipe(self, equipe):
+        if not self.atribuida:
+            return
+        self.equipe = equipe
+
+    def marcar(self):
+        self.invalida = True
+
+    def desmarcar(self):
+        self.invalida = False
